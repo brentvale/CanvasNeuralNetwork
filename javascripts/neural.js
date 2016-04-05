8af619038e098daf2ad1a1ab3ec0191481478fb8
Brent Vale <brentvale@gmail.com>
2016-04-05 12:33:35 -0700

    creates first level of neurons from central neuron

--- javascripts/neural.js
+++ javascripts/neural.js
@@ -1,9 +1,12 @@
+var DELTAS = [[-1,0], [-1,1], [0,1], [1,1], [1,0], [1,-1], [0,-1], [-1,-1]];
 function Neural(options){
   this.ctx = options.context;
   this.width = options.width;
   this.height = options.height;
+  this.neuronsArray = [];
   this.clearScreen();
   this.createNeurons();
+  
 };
 
 Neural.prototype = {
@@ -15,5 +18,27 @@ Neural.prototype = {
   },
   createNeurons: function(){
     
+    //center neuron
+    var firstNeuron = new Neuron({x:(this.width/2), y:(this.height/2), context:this.ctx, level: 0});
+    
+    
+    this.neuronsArray.push(firstNeuron);
+    
+    //create level one with 6 branches (selected at random)
+    this.neuronsArray.push(new Neuron({parentNeuron: firstNeuron, context:this.ctx, level: 1, deltas: DELTAS[0]}));
+    this.neuronsArray.push(new Neuron({parentNeuron: firstNeuron, context:this.ctx, level: 1, deltas: DELTAS[1]}));
+    this.neuronsArray.push(new Neuron({parentNeuron: firstNeuron, context:this.ctx, level: 1, deltas: DELTAS[7]}));
+    this.neuronsArray.push(new Neuron({parentNeuron: firstNeuron, context:this.ctx, level: 1, deltas: DELTAS[3]}));
+    this.neuronsArray.push(new Neuron({parentNeuron: firstNeuron, context:this.ctx, level: 1, deltas: DELTAS[4]}));
+    this.neuronsArray.push(new Neuron({parentNeuron: firstNeuron, context:this.ctx, level: 1, deltas: DELTAS[5]}));
+    
+    //iterate through array and draw each
+    for(var i = 0; i < this.neuronsArray.length; i++){
+      this.neuronsArray[i].drawNeuralHead();
+      if(this.neuronsArray[i].parentNeuron){
+        this.neuronsArray[i].drawPathToParent();
+      }
+    }
   },
+  
 };
--- javascripts/neuron.js
+++ javascripts/neuron.js
@@ -1,6 +1,35 @@
-function Neuron(){
-  
+function Neuron(options){
+  this.ctx = options.context;
+  this.color = options.color || "white";
+  this.radius = options.radius || 2;
+  this.level = options.level;
+  this.parentNeuron = options.parentNeuron || null;
+  this.deltas = options.deltas;
+  this.y = options.y || this.setYFromParent();
+  this.x = options.x || this.setXFromParent();
 };
 Neuron.prototype = {
-  
+  drawNeuralHead: function(){
+    this.ctx.beginPath(); 
+    this.ctx.arc(this.x,this.y, this.radius, 0, Math.PI*2); 
+    this.ctx.closePath(); 
+    this.ctx.fillStyle = this.color; 
+    this.ctx.fill(); 
+  },
+  drawPathToParent: function(){
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.x,this.y);
+    this.ctx.lineTo(this.parentNeuron.x,this.parentNeuron.y);
+    this.ctx.strokeStyle = this.color;
+    this.ctx.stroke();
+  },
+  setXFromParent: function(){
+    //[-1, 0]
+    var deltaX = this.parentNeuron.x + 50*this.deltas[0] + Math.floor(Math.random() * 20);
+    return deltaX;
+  },
+  setYFromParent: function(){
+    var deltaY = this.parentNeuron.y + 50*this.deltas[1] + Math.floor(Math.random() * 20);
+    return deltaY;
+  },
 }

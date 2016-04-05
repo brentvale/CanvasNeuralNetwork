29d36f8c5912ddb901042911bf6587e2d26ea27c
Brent Vale <brentvale@gmail.com>
2016-04-05 14:35:21 -0700

    canvas background displays properly

--- javascripts/neural.js
+++ javascripts/neural.js
@@ -1,34 +1,35 @@
 var DELTAS = [[-1,0], [-1,1], [0,1], [1,1], [1,0], [1,-1], [0,-1], [-1,-1]];
 function Neural(options){
   this.ctx = options.context;
-  this.color = "white";
-  this.width = options.width;
-  this.height = options.height;
+  this.color = "#606060";
+  WINDOW_WIDTH = options.width;
+  WINDOW_HEIGHT = options.height;
   this.neuronsArray = [];
   this.clearScreen();
   this.createNeurons();
-  
 };
 
 Neural.prototype = {
   clearScreen: function(){
     this.ctx.beginPath();
-    this.ctx.rect(0, 0, this.width, this.height);
+    this.ctx.rect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
     this.ctx.fillStyle = "black";
     this.ctx.fill();
   },
   createNeurons: function(){
     //center neuron
-    var firstNeuron = new Neuron({x:(this.width/2), y:(this.height/2), level: 0});
+    var firstNeuron = new Neuron({x:(WINDOW_WIDTH/2), y:(WINDOW_HEIGHT/2), level: 0});
     this.neuronsArray.push(firstNeuron);
 
     //create level one with 6 branches (selected at random)
     this.neuronsArray.push(new Neuron({parentNeuron: firstNeuron, level: 1, deltas: DELTAS[0]}));
     this.neuronsArray.push(new Neuron({parentNeuron: firstNeuron, level: 1, deltas: DELTAS[1]}));
-    this.neuronsArray.push(new Neuron({parentNeuron: firstNeuron, level: 1, deltas: DELTAS[7]}));
+    this.neuronsArray.push(new Neuron({parentNeuron: firstNeuron, level: 1, deltas: DELTAS[2]}));
     this.neuronsArray.push(new Neuron({parentNeuron: firstNeuron, level: 1, deltas: DELTAS[3]}));
     this.neuronsArray.push(new Neuron({parentNeuron: firstNeuron, level: 1, deltas: DELTAS[4]}));
     this.neuronsArray.push(new Neuron({parentNeuron: firstNeuron, level: 1, deltas: DELTAS[5]}));
+    this.neuronsArray.push(new Neuron({parentNeuron: firstNeuron, level: 1, deltas: DELTAS[6]}));
+    this.neuronsArray.push(new Neuron({parentNeuron: firstNeuron, level: 1, deltas: DELTAS[7]}));
     
     //iterate through array and draw each
     for(var i = 0; i < this.neuronsArray.length; i++){
@@ -38,23 +39,39 @@ Neural.prototype = {
     for(var i = 0; i < this.neuronsArray.length; i++){
       var temp = this.neuronsArray[i];
       this.drawNeuralHead(temp);
+      this.drawChildren(temp);
       if(this.neuronsArray[i].parentNeuron){
         this.drawPathToParent(temp);
       }
     }  
   },
+  drawChildren: function(temp){
+    if(temp.childNeurons.length == 0){
+      return;
+    }
+    for(var i = 0; i < temp.childNeurons.length; i++){
+      this.drawChildren(temp.childNeurons[i]);
+      this.drawNeuralHead(temp);
+      this.drawPathToParent(temp);
+    }
+  },
   drawPathToParent: function(temp){
-    this.ctx.beginPath();
-    this.ctx.moveTo(temp.x,temp.y);
-    this.ctx.lineTo(temp.parentNeuron.x,temp.parentNeuron.y);
-    this.ctx.strokeStyle = temp.color;
-    this.ctx.stroke();
+    if(temp.parentNeuron){
+      this.ctx.beginPath();
+      this.ctx.moveTo(temp.x,temp.y);
+      this.ctx.lineTo(temp.parentNeuron.x,temp.parentNeuron.y);
+      // this.ctx.quadraticCurveTo(temp.parentNeuron.x, 10, 388, 10, 388, temp.parentNeuron.y);
+      this.ctx.strokeStyle = temp.color;
+      this.ctx.stroke();
+    }
   },
   drawNeuralHead:function(temp){
-    this.ctx.beginPath(); 
-    this.ctx.arc(temp.x,temp.y, temp.radius, 0, Math.PI*2); 
-    this.ctx.closePath(); 
-    this.ctx.fillStyle = this.color; 
-    this.ctx.fill(); 
+    if(temp.parentNeuron){
+      this.ctx.beginPath(); 
+      this.ctx.arc(temp.x,temp.y, temp.radius, 0, Math.PI*2); 
+      this.ctx.closePath(); 
+      this.ctx.fillStyle = this.color; 
+      this.ctx.fill(); 
+    }
   }
 };
--- javascripts/neuron.js
+++ javascripts/neuron.js
@@ -1,5 +1,5 @@
 function Neuron(options){
-  this.color = options.color || "white";
+  this.color = options.color || "#424242";
   this.radius = options.radius || 2;
   this.level = options.level;
   this.childNeurons = [];
@@ -12,26 +12,36 @@ Neuron.prototype = {
   
   setXFromParent: function(){
     //[-1, 0]
-    var deltaX = this.parentNeuron.x + 50*this.deltas[0] + Math.floor(Math.random() * 20);
-    return deltaX;
+    if(this.deltas){
+      var deltaX = this.parentNeuron.x + 40*this.deltas[0] + Math.floor(Math.random() * 100 - 50);
+      return deltaX;
+    }
   },
   setYFromParent: function(){
-    var deltaY = this.parentNeuron.y + 50*this.deltas[1] + Math.floor(Math.random() * 20);
-    return deltaY;
-  },
-  addChild: function(child){
-    this.childNeurons.push(child);
+    if(this.deltas){
+      var deltaY = this.parentNeuron.y + 40*this.deltas[1] + Math.floor(Math.random() * 100 - 50);
+      return deltaY;
+    }
   },
   createChildren: function(){
-    if(this.level == 10){
+    if(this.x < -50 || this.y < -50 || this.x > (WINDOW_WIDTH + 50) || this.y > (WINDOW_HEIGHT + 50)){
       return;
-    };
-    var children = [];
+    } 
+    if(this.level > 15){
+      return;
+    }
     //create between 1 and 8 children
-    var totalChildren = Math.floor(Math.random() * 2);
+    var totalChildren = 2;
+    if(this.level > 5){
+      totalChildren = 1;
+    } else {
+      totalChildren = Math.floor(Math.random() * 3) + 1;
+    }
+    
     for(var i = 0; i < totalChildren; i++){
       var newChild = new Neuron({parentNeuron: this, level: (this.level + 1), deltas: this.deltas});
-      this.addChild(newChild);
+      newChild.createChildren();
+      this.childNeurons.push(newChild);
     }
   }
 }
